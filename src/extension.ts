87540f8b524494a6456330278cb95a1a6e0ca7fc
Juanjuan <liujuanjuan134@foxmail.com>
2020-05-30 12:54:15 +0800

    添加保存lua文件时诊断

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,23 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {
+	commands,
+	window,
+	workspace,
+	languages,
+	ExtensionContext,
+	Diagnostic,
+	DiagnosticCollection,
+	DiagnosticSeverity,
+	Range,
+	TextDocument
+} from 'vscode';
+
+let diagnosticCollection: DiagnosticCollection;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,14 +26,38 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fancystudiolua.helloWorld', () => {
+	let disposable = commands.registerCommand('fancystudiolua.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FancyStudioLua!');
+		window.showInformationMessage('Hello World from FancyStudioLua!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	workspace.onDidSaveTextDocument((text: TextDocument) => {
+		if (text.languageId === 'lua'){
+			diagnosticCollection.clear();
+			diagnosticCollection.set(text.uri, fsCheckLuaFormat(text));
+		}
+	});
+
+	diagnosticCollection = languages.createDiagnosticCollection('lua');
+	context.subscriptions.push(diagnosticCollection);
+}
+
+function fsCheckLuaFormat(document: TextDocument) {
+	console.log('Congratulations, "fancystudiolua" fsCheckLuaFormat!');
+
+	// diagnostics
+	let diagnostics = [];
+	let d: Diagnostic = {
+		severity: DiagnosticSeverity.Warning,
+		range: new Range(document.positionAt(0), document.positionAt(0)),
+		message: "test Diagnostic"
+	};
+	diagnostics.push(d);
+	return diagnostics
 }
 
 // this method is called when your extension is deactivated
